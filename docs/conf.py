3de27e534eb070e32801fe1a8ee00a6fb3e4499e
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-02-25 17:51:43 -0300

    Commit to test webhook

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ author = 'HURIDOCS'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
     'recommonmark',
     'sphinx_rtd_theme'
